1f88808b8407f3700505f1a435654a8e935112c7
Paulo Teixeira <hst.paulo@gmail.com>
2020-10-11 00:40:13 -0300

    Delete aula015.py

--- aula015.py
+++
@@ -1,7 +0,0 @@
-nome = 'José'
-idade = 33
-salário = 987.35
-
-print(f'O {nome} tem {idade} anos e ganha R$ {salário:.2f}')
-print(f'O {nome} tem {idade} anos.') # novo
-print('O {} tem {} anos.'.format(nome,idade))
